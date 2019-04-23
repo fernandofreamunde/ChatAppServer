25f232a52d2fc67d7a6a999c9735af36860fdbaa
Fernando Andrade <fernandofreamunde@gmail.com>
2019-04-23 04:31:46 +0200

    :recycle: make register respond with json

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,14 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Security\LoginFormAuthenticator;
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -29,11 +27,12 @@ class SecurityController extends AbstractController
     /**
      * @Route("/register", name="app_register", methods={"POST"})
      */
-    public function register(Request $request, UserPasswordEncoderInterface $encoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator)
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
     {
         $user = new User();
         $user->setEmail($request->request->get('email'))
             ->setPassword($encoder->encodePassword($user, $request->request->get('password')));
+        $user->setUsername($request->request->get('username'));
 
         try {
             $em = $this->getDoctrine()->getManager();
@@ -44,14 +43,10 @@ class SecurityController extends AbstractController
             if ($exception instanceof UniqueConstraintViolationException) {
                 return new JsonResponse(["error" => "Email in use."], 422, ['Content-Type' => 'application/json']);
             }
+            return new JsonResponse(["message" => "Something went wrong."], 500, ['Content-Type' => 'application/json']);
         };
 
-        return $guardHandler->authenticateUserAndHandleSuccess(
-            $user,
-            $request,
-            $authenticator,
-            'main'
-        );
+        return new JsonResponse(["message" => "User created."], 201, ['Content-Type' => 'application/json']);
     }
 
     /**
