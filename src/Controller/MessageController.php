ea4219ae060b45fe4c90b16bb15a818896496218
Fernando Andrade <fernandofreamunde@gmail.com>
2019-04-24 23:45:03 +0200

    :sparkles: new messsage endpoint

---
+++ src/Controller/MessageController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Conversation;
+use App\Entity\Message;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+
+class MessageController extends AbstractController
+{
+    /**
+     * @Route("/conversation/{id}/message", name="new_message", methods={"POST"})
+     */
+    public function new(Conversation $conversation, Request $request, Security $security)
+    {
+        $message = new Message();
+        $message->setAuthor($security->getUser());
+        $message->setConversation($conversation);
+        $message->setContent($request->request->get('message'));
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($message);
+        $em->flush();
+
+        //had to use this because of a circular reference,
+        //most of the code is the $this->>json() that is normally used in this app
+        $json = $this->container->get('serializer')->serialize([ 'message' => $message], 'json', array_merge([
+            'json_encode_options' => JsonResponse::DEFAULT_ENCODING_OPTIONS,
+            'circular_reference_handler' => function ($object) {
+                return $object->getId();
+            }
+        ], [
+            'groups' => ['list']
+        ]));
+        return new JsonResponse($json, 201, [], true);
+
+        return $this->json([
+            'message' => $message,
+        ], 201, [], [
+            'groups' => ['list'],
+        ]);
+    }
+}
